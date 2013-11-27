65e3aeaeab73b279dffbb7b0df4e14ddf5ae2854
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2013-11-27 19:25:04 +0000

    Adding things for rootcore compatibility

--- python/LoadLibraries.py
+++ python/LoadLibraries.py
@@ -14,6 +14,16 @@ def loadLibrary(lib) :
         sys.exit(-1)
         
     try:
+        import os
+        if os.getenv('ROOTCOREDIR') :
+            from ROOT import gROOT
+            if lib in ROOT.gSystem.GetLibraries() :
+                print 'skipping loading!'
+                return
+            print 'libs:',ROOT.gSystem.GetLibraries()
+            gROOT.ProcessLine (".x $ROOTCOREDIR/scripts/load_packages.C");
+            print 'Loaded ALL libraries (during attempt %s)'%lib
+            return
         #do not load libraries multiple times
         libs = ROOT.gSystem.GetLibraries()
         l_so = lib+'.so'
