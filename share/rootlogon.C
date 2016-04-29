6f3901122c4d12dc5cd3775f9b05e8864957a606
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2016-04-29 13:57:49 +0000

    turn off text output

--- share/rootlogon.C
+++ share/rootlogon.C
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "TROOT.h"
+#include "TInterpreter.h"
 #include "AtlasStyleColor.h"
 #include "Extras.h"
+
 
 void rootlogon()
 {
@@ -13,7 +15,6 @@ void rootlogon()
   //gROOT->LoadMacro("AtlasStyleColor.C");
   //gROOT->LoadMacro("Extras.C");
   SetAtlasStyle();
-  ListExtras();
   set_plot_style_kurt();
 
   gROOT->SetBatch(false);
@@ -21,7 +22,8 @@ void rootlogon()
   //std::cout << "# rootlogon.C: SetDefaultSumw2(kTRUE); // Thanks Rene Brun" << std::endl;
   //TH1::SetDefaultSumw2(kTRUE);
 
-  std::cout << "# rootlogon.C: " << "gStyle->SetPaintTextFormat(\"4.3f \")" << std::endl;
-  std::cout << "# rootlogon.C: " << "To turn off title: gStyle->SetOptTitle(0);" << std::endl;
-  std::cout << "# rootlogon.C: " << "For Z axis: gStyle->SetPadRightMargin(0.16);" << std::endl;
+//   std::cout << "# rootlogon.C: " << "gStyle->SetPaintTextFormat(\"4.3f \")" << std::endl;
+//   std::cout << "# rootlogon.C: " << "To turn off title: gStyle->SetOptTitle(0);" << std::endl;
+//   std::cout << "# rootlogon.C: " << "For Z axis: gStyle->SetPadRightMargin(0.16);" << std::endl;
+  std::cout << "Type ListExtras() to see extra macros." << std::endl;
 }
